a88c03b892ac8d61d3196fdb0c6c2979e8c68bbf
odls <alexeatapple@yahoo.com.tw>
2021-06-13 14:48:43 +0800

    建立 PuyoData

---
+++ Assets/Scripts/Data/PuyoData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum E_PUYO_TYPE {
+	Wall = -2,
+	Empty = -1,
+	Red,
+	Yellow,
+	Green,
+	Blue,
+	Len
+}
+
+[CreateAssetMenu]
+public class PuyoData : ScriptableObject {
+	[SerializeField] E_PUYO_TYPE mType;
+	public E_PUYO_TYPE type => mType;
+
+	[SerializeField] Sprite[] sprites;
+	public Sprite GetSpriteByFlag(int p_linkFlag) {
+		if (p_linkFlag >= sprites.Length) {
+			return null;
+		} else {
+			return sprites[p_linkFlag];
+		}
+	}
+}
