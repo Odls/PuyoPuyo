93f42acea01c527d5f5c10fdd73a386b8d07e464
odls <alexeatapple@yahoo.com.tw>
2021-06-13 15:54:49 +0800

    在 PuyoView Prefab 顯示圖片

---
+++ Assets/Scripts/Test/StepTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepTest : MonoBehaviour{
+	[SerializeField] PuyoView puyoView;
+	private IEnumerator Start() {
+		WaitForSeconds _wait = new WaitForSeconds(0.1f);
+		while (true) {
+			var _type = PuyoManager.instance.GetRandomType();
+			puyoView.SetType(_type);
+			yield return _wait;
+		}
+	}
+}
---
+++ Assets/Scripts/View/PuyoView.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuyoView : MonoBehaviour {
+	[SerializeField] SpriteRenderer renderer;
+	protected virtual int linkFlag => 0;
+	public E_PUYO_TYPE type { get; protected set; }
+	public void SetType(E_PUYO_TYPE p_type, bool p_autoRefresh = true) {
+		type = p_type;
+		if (p_autoRefresh) { Refresh(); }
+	}
+	public void Refresh() {
+		PuyoData _puyoData = PuyoManager.instance.GetData(type);
+		renderer.sprite = _puyoData.GetSpriteByFlag(linkFlag);
+	}
+}
