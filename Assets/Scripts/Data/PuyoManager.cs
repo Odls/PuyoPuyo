7a00b8dfb29d098eeafa867d37be64810a60b5a3
odls <alexeatapple@yahoo.com.tw>
2021-06-13 15:33:48 +0800

    在場景中放置 PuyoManager 單例

---
+++ Assets/Scripts/Data/PuyoManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuyoManager : ManagerBase<PuyoManager> {
+	[SerializeField] List<PuyoData> puyoDatas = new List<PuyoData>();
+
+	Dictionary<E_PUYO_TYPE, PuyoData> puyoDataDict = new Dictionary<E_PUYO_TYPE, PuyoData>();
+
+	protected override void Awake() {
+		base.Awake();
+
+		foreach (var _data in puyoDatas) {
+			puyoDataDict.Add(_data.type, _data);
+		}
+	}
+
+	public PuyoData GetData(E_PUYO_TYPE p_type) {
+		PuyoData _data;
+		if (puyoDataDict.TryGetValue(p_type, out _data)) {
+			return _data;
+		} else {
+			Debug.LogError($"No {p_type} In puyoDataDict");
+			return null;
+		}
+	}
+
+	public E_PUYO_TYPE GetRandomType() {
+		return (E_PUYO_TYPE)Random.Range(0, (int)E_PUYO_TYPE.Len);
+	}
+}
---
+++ Assets/Scripts/Util/ManagerBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ManagerBase<T> : MonoBehaviour where T : ManagerBase<T> {
+	public static T instance { get; private set; }
+
+	protected virtual void Awake() {
+		if (instance != null) {
+			Destroy(gameObject);
+		} else {
+			instance = (T)this;
+		}
+	}
+}
